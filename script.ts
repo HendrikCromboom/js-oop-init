8658973955313901c4bd49a17ed4fdc6562ac9ee
HENDRIK <hendrik@KALI.home>
2020-09-17 02:03:53 +0200

    init

---
+++ script.ts
@@ -0,0 +1,12 @@
+function Story(title, name, language, duration){
+    this.title = title;
+    this.name = name;
+    this.language = language;
+    this.duration = duration;
+}
+Story.prototype.tellStory = function (){
+    return `${this.title}! My name is ${this.name}, I have been learning ${this.language} for about ${this.duration}...`;
+}
+const story1 = new Story("Hello World", "Hendrik", "JavaScript", "1 months")
+alert(story1.tellStory())
+
